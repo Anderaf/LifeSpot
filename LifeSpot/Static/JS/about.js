e0cbfbf739e3752e1f4c0d118c24df600411a5b1
Anderaf <kuldperea@gmail.com>
2023-06-08 12:26:52 +1000

    31.4 Slider

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -15,7 +15,7 @@
         `<p>${reviewText}</p>` +
         `</div>`;
 }*/
-function getReview() {
+/*function getReview() {
     let review = {}
 
     review["userName"] = prompt("Как вас зовут ?")
@@ -37,4 +37,92 @@ const writeReview = review => {
         `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
         `<p>${review['comment']}</p>` +
         '</div>';
+}*/
+function Comment() {
+    // Запросим имя
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
+        return
+    }
+
+    // Запросим текст
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
+        return
+    }
+
+    // Сохраним текущее время
+    this.date = new Date().toLocaleString()
+}
+function addComment() {
+
+    let comment = new Comment()
+
+    if (comment.empty) {
+        return;
+    }
+
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        let review = Object.create(comment)
+        review.rate = 0;
+
+        writeReview(review)
+    } else {
+        writeReview(comment)
+    }
+}
+const writeReview = review => {
+    let likeCounter = '';
+
+    if (review.hasOwnProperty('rate')) {
+
+        let commentId = Math.random();
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' + `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
+}
+function addLike(id) {
+    let element = document.getElementById(id);
+
+    let array = element.innerText.split(' ')
+
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    resultNum += 1
+
+    array[array.length - 1] = `${resultNum}`
+
+    element.innerText = array.join(' ')
+}
+
+var currentImg = 0;
+function changeSlide(n) {
+    var imgs = document.querySelectorAll('.slider img');
+    var dots = document.querySelectorAll('.dot');
+    console.log(imgs)
+    console.log(dots)
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].style.opacity = 0;
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    currentImg = n;
+
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+}
+function nextSlide() {
+    var imgs = document.querySelectorAll('.slider img');
+    if (currentImg < imgs.length - 1) {
+        changeSlide(currentImg + 1)
+    }
+}
+function previousSlide() {
+    if (currentImg > 0) {
+        changeSlide(currentImg - 1)
+    }
 }
--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,2 +1,10 @@
 let elements = document.getElementsByTagName('*');
 alert(`Total amount of elements on the page: ${elements.length}`)
+const saveInput = function () {
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    alert('Последний ввод: ' + this.lastInput
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    this.lastInput = currentInput;
+}
